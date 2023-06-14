ed35d011b07906972457d8caa8fdfa3d4a8207a7
Kira <130936217+KiraBalabanova@users.noreply.github.com>
2023-06-14 07:42:33 +0100

    added links to Local Projects

--- src/pages/FriendlyLocalProjects.tsx
+++ src/pages/FriendlyLocalProjects.tsx
@@ -11,7 +11,7 @@ export const FriendlyLocalProjects = () => {
       <ListItem>
         <Link
           color="blue.500"
-          href="//www.thepetitionsite.com/472/269/886/save-our-st-george-pool-shadwell/"
+          href="https://www.change.org/p/tower-hamlets-council-save-our-st-george-pool-shadwell"
           isExternal
         >
           Save St George’s Swimming Pool in Wapping
@@ -20,15 +20,97 @@ export const FriendlyLocalProjects = () => {
       <ListItem>
         <Link
           color="blue.500"
-          href="//poplarneighbourhoodplanningforum.com"
+          href="http://romanroadbowneighbourhoodplan.org/"
           isExternal
         >
           Poplar Neighbourhood Planning Forum
         </Link>
       </ListItem>
       <ListItem>
-        <Link color="blue.500" href="https://romanroadtrust.co.uk" isExternal>
+        <Link color="blue.500" href="https://romanroadtrust.co.uk/" isExternal>
           Roman Road Trust
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://www.lovemystreet.uk/" isExternal>
+          Love My Street
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://www.mumsforlungs.org/" isExternal>
+          Mums for Lungs
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://www.instagram.com/washwombles/" isExternal>
+          Wash Wombles
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://poplargreenfutures.co.uk/" isExternal>
+          Poplar Green Futures
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="http://www.elgs.org.uk/" isExternal>
+          East London Garden Society
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="http://poplarneighbourhoodplanningforum.com/" isExternal>
+          Poplar Neighbourhood Planning Forum
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://www.instagram.com/lowerregentscoalition/" isExternal>
+          Lower Regents Coalition
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://heartbg.uk/" isExternal>
+          Save Our Safer Streets in Bethnal Green
+        </Link>
+      </ListItem>
+  
+      <ListItem>
+        <Link color="blue.500" href="//betterstreets4towerhamlets.org.uk" isExternal>
+          Better Streets for Tower Hamlets
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="//www.livingstreets.org.uk/groups/tower-hamlets" isExternal>
+          Tower Hamlets Living Streets Group
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="//towerhamletswheelers.org.uk" isExternal>
+          Tower Hamlets Wheelers
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://www.friendsofmeathgardens.org/" isExternal>
+          Friends of Meath Gardens
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://www.rfsk.org.uk/events" isExternal>
+          Royal Foundation of St Katharine
+        </Link>
+      </ListItem>
+      
+      <ListItem>
+        <Link color="blue.500" href="http://hackneyandtowerhamletsfoe.org.uk/" isExternal>
+          Friends of the Earth Hackney and Tower Hamlets
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://fomep.org.uk/" isExternal>
+          Friends of Mile End Park
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link color="blue.500" href="https://www.instagram.com/socialstreetsuk/" isExternal>
+          Social Streets
         </Link>
       </ListItem>
     </List>
