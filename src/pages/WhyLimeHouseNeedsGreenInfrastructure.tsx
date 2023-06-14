3b226b0f7f51171ca336fe0ea609b8b454cb05ce
Kira <130936217+KiraBalabanova@users.noreply.github.com>
2023-06-14 11:57:15 +0100

    Edited tags on Green Infrastructure

--- src/pages/WhyLimeHouseNeedsGreenInfrastructure.tsx
+++ src/pages/WhyLimeHouseNeedsGreenInfrastructure.tsx
@@ -4,7 +4,6 @@ import {
   Stack,
   Card,
   CardBody,
-  Stack,
 } from "@chakra-ui/react";
 
 export const WhyLimeHouseNeedsGreenInfrastructure = () => {
@@ -13,7 +12,6 @@ export const WhyLimeHouseNeedsGreenInfrastructure = () => {
       <CardBody>
         <Stack spacing="8">
           <Box>
-            
             <Text pt="2" fontSize="md">
             We aim to develop and implement an evidence-based, strategic approach to tackle pollution in Limehouse. Simply planting trees is not a sufficient solution, as planting them on the wrong side of the road can increase pollution. Improving air quality is a complex process that requires expertise. To this end, we have reached out to ARUP, a team of global air pollution experts, who are willing to conduct research in the area and provide recommendations for a master plan. While this plan would cost £50K, it is necessary for the improvement of air quality in the area. We believe that the residents of Limehouse deserve a safe and healthy environment, and we want to empower them by returning agency to the community. We recognize that Tower Hamlets Council has a budget for tackling pollution, but they are not taking sufficient action. Therefore, we need to work together to implement an evidence-based strategy for tackling pollution in Limehouse. 
             </Text>
